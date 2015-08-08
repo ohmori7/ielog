f7e08587d9d58db84ed1522332e95ba332a7e770
Motoyuki OHMORI <ohmori@tottori-u.ac.jp>
2015-08-08 16:32:31 +0900

    renumbering in a list of houses.

--- list.php
+++ list.php
@@ -7,7 +7,7 @@
 <h1><img alt="logo" src="logo.png" />家ログ 一覧</h1>
 <ol>
 <?php
-for ($i = 0; $i < 10; $i++) {
+for ($i = 1; $i < 11; $i++) {
 	echo("<li><a href=\"detail.php&id=$i\">おばーちゃん画像$i 家の画像$i " .
 	    "情報$i</a></li>\n");
 }
