93b7a4d5be74530b85da4b3d29f73bae5df75b5d
Motoyuki OHMORI <ohmori@tottori-u.ac.jp>
2015-08-08 16:13:08 +0900

    update list.

--- list.php
+++ list.php
@@ -6,12 +6,12 @@
 <body>
 <h1><img src="logo.png" />家ログ 一覧</h1>
 <ol>
-<li>hogehoge 1</li>
-<li>hogehoge 2</li>
-<li>hogehoge 3</li>
-<li>hogehoge 4</li>
-<li>hogehoge 5</li>
-<li>hogehoge 6</li>
-</ol>
+<li>おばーちゃん画像1 家の画像1 情報1</li>
+<li>おばーちゃん画像2 家の画像2 情報2</li>
+<li>おばーちゃん画像3 家の画像3 情報3</li>
+<li>おばーちゃん画像4 家の画像4 情報4</li>
+<li>おばーちゃん画像5 家の画像5 情報5</li>
+<li>おばーちゃん画像6 家の画像6 情報6</li>
+<li>おばーちゃん画像7 家の画像7 情報7</li>
 </body>
 </html>
