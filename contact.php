7fe76939005833c503828b28ac01ba10879dedca
Motoyuki OHMORI <ohmori@tottori-u.ac.jp>
2015-08-08 16:20:16 +0900

    add contact page.

---
+++ contact.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<title>家ログ</title>
+</head>
+<body>
+<h1><img src="logo.png" />家ログ 問い合わせ</h1>
+<p>
+<form action="contact.php">
+お名前: <input type="text" value="" />
+メールアドレス: <input type="text" value="" />
+メッセージ: <textarea value="" width="30" height="20" />
+<input type="button" value="送信" />
+</form>
+</p>
+</body>
+</html>
