d41c04ea7e6ebbe84e5e922afee81a33ad7e1992
Motoyuki OHMORI <ohmori@tottori-u.ac.jp>
2015-08-08 21:54:42 +0900

    brush up style sheet.

--- detail.php
+++ detail.php
@@ -2,16 +2,19 @@
 require_once('lib.php');
 header_print('家ログ 詳細', array());
 
-echo('<a href="contact.php?id=XXXX">contact</a>');
+$id = '1'; /* XXX */
+echo("<a href=\"contact.php?id=$id\">contact</a>");
 
 $ncomments = 10; /* XXX */
 for ($i = 0; $i < $ncomments; $i++) {
-	echo("<div class=\"container\" >\n");
+	$imgfile = "images/star${id}.png";	// XXX
+	echo("<div class=\"container\">\n");
 	echo("<div class=\"commentator\">\n");
-	echo("<img alt=\"fugafuga\" src=\"hogehoge\" />\n");
+	echo("<img alt=\"fugafuga\" src=\"hogehoge\" />");
 	echo("</div>\n");
 	echo("<div class=\"comment\">");
 	echo('hogehoge');
+	echo("<img alt=\"score\" src=\"$imgfile\" />");
 	echo("</div>\n");
 	echo("</div>\n");
 }
