04bba9ef41eca756967a5947465654b27ed7cad2
Motoyuki OHMORI <ohmori@tottori-u.ac.jp>
2015-08-08 16:02:07 +0900

    update index page.

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-<title></title>
+<title>家ログ</title>
 </head>
 <body>
-<h1></h1>
-<p>
+<h1><img src="logo.png" />家ログ</h1>
+<ul>
+<li>ホーム</li>
+<li>口コミ</li>
+<li></li>
+</ul>
 </body>
 </html>
