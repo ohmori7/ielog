cae4a71bc5024b62999b8dccd5ec78a9274339d0
Motoyuki OHMORI <ohmori@tottori-u.ac.jp>
2015-08-08 17:17:34 +0900

    fix the layout.

--- contact.php
+++ contact.php
@@ -2,14 +2,14 @@
 require_once('lib.php');
 header_print('家ログ 問い合わせ', array());
 ?>
-<h1><img alt="logo" src="logo.png" />
 <p>
 <form action="contact.php">
-お名前: <input type="text" value="" />
-メールアドレス: <input type="text" value="" />
-メッセージ: <textarea value="" width="30" height="20" />
+お名前: <input type="text" value="" /><br />
+メールアドレス: <input type="text" value="" /><br />
+メッセージ: <textarea value="" width="30" height="20"></textarea><br />
 <input type="button" value="送信" />
 </form>
+</p>
 <?php
 footer_print();
 ?>
