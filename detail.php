9442a2f29d60d09e992b697b8d3e76f291793fb1
Motoyuki OHMORI <ohmori@tottori-u.ac.jp>
2015-08-08 22:50:13 +0900

    brush up a detailed page.

--- detail.php
+++ detail.php
@@ -13,18 +13,18 @@ echo("<img alt=\"$appear\" src=\"$appear\" height=\"200px\" />\n");
 echo("<a href=\"contact.php?id=$id\">○○さんに問い合わせ</a>");
 
 $ncomments = 10; /* XXX */
+echo("<div class=\"balloon-wrapper\">\n");
 for ($i = 0; $i < $ncomments; $i++) {
 	$imgfile = "images/star${id}.png";	// XXX
-	echo("<div class=\"container\">\n");
-	echo("<div class=\"commentator\">\n");
-	echo("<img alt=\"fugafuga\" src=\"hogehoge\" />");
-	echo("</div>\n");
-	echo("<div class=\"comment\">");
-	echo('hogehoge');
+	if($i % 2 == 0)
+		echo("<p class=\"balloon-left\">");
+	else
+		echo("<p class=\"balloon-right\">");
+	echo("また、資源をエネルギーに変換するためには、要らないものを空気中に排出しなくてはなりません。二酸化炭素などの排出物は、大気汚染を引き起こし、やがては地球全体の気温を徐々にあげてしまいます。これが地球温暖化と呼ばれるもので、異常気象や農作物への影響、海水面の上昇、紫外線の問題など、さまざまな問題の原因となっています。人間だけでなく、すべての生き物の住みやすい環境を守るためにも、エネルギーの節約は必要なのです。 <br />");
 	echo("<img alt=\"score\" src=\"$imgfile\" />");
-	echo("</div>\n");
-	echo("</div>\n");
+	echo("</p>\n");
 }
+echo("</div>\n");
 
 footer_print();
 ?>
