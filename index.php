2a07daa30f78d4159f23d410aecdfabd6448b823
Motoyuki OHMORI <ohmori@tottori-u.ac.jp>
2015-08-09 15:12:00 +0900

    mark up first initial page.

--- index.php
+++ index.php
@@ -2,7 +2,10 @@
 require_once('lib.php');
 header_print('家ログ', array());
 ?>
-hogehoge
+<div style="text-align: center">
+家ログとは？ ：人と人をつなぐ空き家紹介サービス<br />
+<img src="images/logo.png" />
+</div>
 <?php
 footer_print();
 ?>
