967b21648373b1fb03f9759f6e7c2a557ecbe053
Motoyuki OHMORI <ohmori@tottori-u.ac.jp>
2015-12-27 01:16:58 +0000

    fix links to files, and output actually uploaded image file, not a dummy.

--- realestate/detail.php
+++ realestate/detail.php
@@ -1,9 +1,11 @@
 <?php
+require_once('../lib.php');
 require_once('lib.php');
 header_print('家ログ 詳細', array());
 
 $id = param_get('id');
-if (empty($id)) {
+$r = db_record_get('realestate', 'id', $id);
+if ($r === false) {
 	echo('ERROR: wrong page transition!!');
 	footer_print();
 	die();
@@ -23,8 +25,8 @@ if (empty($id)) {
 			<h2>詳細情報</h2>
 			<div class="detail_photo">
 <?php
-$owner = "images/owner${id}.png";
-$appear = "images/appear${id}.png";
+$owner = "../images/owner${id}.png";
+$appear = realestate_image_top($r);
 
 echo("<h3>概観</h3>\n");
 echo("<img alt=\"$appear\" src=\"$appear\" width=\"250px\" />\n");
@@ -33,9 +35,9 @@ echo("<img alt=\"$owner\" src=\"$owner\" width=\"250px\" />\n");
 ?>
 			</div>
 			<div class="detail_msg">
-<script type="text/javascript" src="scripts/rendering-mode.js"></script>
-<!--[if IE]><script type="text/javascript" src="scripts/excanvas.js"></script><![endif]-->
-<script type="text/javascript" src="scripts/radar.js"></script>
+<script type="text/javascript" src="../scripts/rendering-mode.js"></script>
+<!--[if IE]><script type="text/javascript" src="../scripts/excanvas.js"></script><![endif]-->
+<script type="text/javascript" src="../scripts/radar.js"></script>
 <script type="text/javascript">
 window.onload = function() {
 	var rc = new html5jp.graph.radar("chart");
@@ -52,16 +54,18 @@ window.onload = function() {
 </script>
 			<h3>評価</h3>
 <div><canvas width="400" height="300" id="chart"></canvas></div>
+			<h3>概要</h3>
+<span class="detail_text">
+<?php echo($r['abstract']); ?>
+</span>
 			<h3>説明</h3>
 <span class="detail_text">
-　夜空にはたくさんの星が輝いています。しかし、これらの美しい星は永遠に輝き続けるわけではありません。星にも一生があり、生まれたり死んだりしています。ただし、星の一生は人間の一生とはくらべものにならないほど長く、その寿命は人間の一億倍もあります。<br/>
-　宇宙のあちこちには、冷たいガスやチリが集まった暗黒星雲と呼ばれるものがあります。暗黒星雲は、その名のとおり光を持たない黒い雲です。実は、この暗黒星雲の中には星の元になる物質がたくさんあります。何かのきっかけで暗黒星雲が縮み始めると、ガスやチリが集まってうずを巻きはじめます。このうず巻きが星のたまごです。 <br/>
-　重力によって更に縮んでいくと、うず巻きの中心部が濃くなり、熱くなっていきます。それは、やがて自ら光を放つようになり、赤ちゃん星が誕生します。冬の夜、肉眼でも見ることができるオリオン座の大星雲も、新しい星が生まれている場所です。星雲の中で、数え切れないほどたくさんの星の赤ちゃんが輝いています。 <br/>
+<?php echo($r['description']); ?>
 </span>
 			</div>
 			<div style="clear:both;">
 				<h3>みんなの評価</h3>
-				<img alt="score" src="images/star3.png" />　　<span style="font-size:15px; font-style:bold; color: #0000FF;">いいね　　おきにいり</span>
+				<img alt="score" src="../images/star3.png" />　　<span style="font-size:15px; font-style:bold; color: #0000FF;">いいね　　おきにいり</span>
 			</div>
             </li>
         	<!-- 写真 -->
@@ -86,13 +90,13 @@ $ncomments = 6; /* XXX */
 echo("<div class=\"balloon-wrapper\">\n");
 for ($i = 0; $i < $ncomments; $i++) {
 	$msg = "また、資源をエネルギーに変換するためには、要らないものを空気中に排出しなくてはなりません。二酸化炭素などの排出物は、大気汚染を引き起こし、やがては地球全体の気温を徐々にあげてしまいます。これが地球温暖化と呼ばれるもので、異常気象や農作物への影響、海水面の上昇、紫外線の問題など、さまざまな問題の原因となっています。人間だけでなく、すべての生き物の住みやすい環境を守るためにも、エネルギーの節約は必要なのです。 <br />";
-	 $msg .= "<img alt=\"score\" src=\"images/star${id}.png\" />"; // XXX
+	 $msg .= "<img alt=\"score\" src=\"../images/star${id}.png\" />"; // XXX
 	if($i % 2 == 0) {
-		echo("<img src=\"./images/icon1.png\" class=\"balloon-left-img\" />\n");
+		echo("<img src=\"../images/icon1.png\" class=\"balloon-left-img\" />\n");
 		echo("<p class=\"balloon-left\">$msg</p>\n");
 	} else {
 		echo("<p class=\"balloon-right\">$msg</p>\n");
-		echo("<img src=\"./images/icon1.png\" class=\"balloon-right-img\" />\n");
+		echo("<img src=\"../images/icon1.png\" class=\"balloon-right-img\" />\n");
 	}
 	echo("<p class=\"clear-p\">&nbsp;</p>\n");
 }
@@ -103,7 +107,7 @@ echo("</div>\n");
     </div>
 </div>
 <?php
-	echo("<a href=\"contact.php?id=$id\">○○さんに問い合わせ</a>");
+	echo("<a href=\"../contact.php?id=$id\">○○さんに問い合わせ</a>");
 ?>
 
 <?php
