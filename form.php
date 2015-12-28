d7d10e8605e79e6682d82ed154102e7c64d164bb
Motoyuki OHMORI <ohmori@tottori-u.ac.jp>
2015-12-28 00:32:47 +0000

    automatically convert QuickForm date style to database DATE type.

--- form.php
+++ form.php
@@ -14,5 +14,20 @@ class Form extends HTML_QuickForm {
 
 		$this->setRequiredNote('<span style="font-size:80%; color:#ff0000;">*</span><span style="font-size:80%;">必須項目</span>');
 	}
+
+	function
+	exportValues()
+	{
+
+		$values = parent::exportValues();
+		foreach ($values as $key => $value) {
+			$element = $this->getElement($key);
+			if ($element->_type !== 'date')
+				continue;
+			$values[$key] = 
+			    $value['Y'] . '-' . $value['m'] . '-' . $value['d'];
+		}
+		return $values;
+	}
 }
 ?>
