19ad112f7301480685d3f6cfbf5ec40cfa2ac1c7
Motoyuki OHMORI <ohmori@tottori-u.ac.jp>
2016-01-18 17:55:21 +0000

    add favicon image.

--- lib.php
+++ lib.php
@@ -107,6 +107,7 @@ REDIRECTMETA;
   <head>
     <link rel="index" href="./index.php" />
     <link rel="author" href="mailto:null@mobile-ip.org" />$_ielog_csses
+    <link rel="icon" type="image/png" href="images/favicon.png">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />$rmeta
     <title>$title</title>
   </head>
